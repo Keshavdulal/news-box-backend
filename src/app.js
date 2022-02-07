9ce5e59fc2456f0b53da8740a9be0cf54e97afaf
keshavdulal <Keshav.dulal@gmail.com>
2022-02-08 00:30:56 +0545

    improve(script): scrape additional data points from news

--- src/app.js
+++ src/app.js
@@ -7,9 +7,16 @@ const puppeteer = require("puppeteer");
   // await page.screenshot({ path: "example.png" });
 
   const newsTitles = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("h3.media__title")).map((i) =>
-      i.innerText.trim()
-    )
+    // scrapes: title, url, summary, category tag and category link
+    Array.from(document.querySelectorAll("div.media__content")).map((i) => {
+      return {
+        title: i.querySelector("h3.media__title")?.innerText?.trim(),
+        url: i.querySelector("h3.media__title")?.children?.[0]?.href,
+        summary: i.querySelector("p.media__summary")?.innerText,
+        categoryTag: i.querySelector("a.media__tag")?.innerText,
+        categoryLink: i.querySelector("a.media__tag")?.href,
+      };
+    })
   );
 
   console.log(newsTitles);
