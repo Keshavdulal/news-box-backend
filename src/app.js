a4a2d6010f566594b98d909523005e95e34b97f9
keshavdulal <Keshav.dulal@gmail.com>
2022-02-08 13:39:52 +0545

    refactor(code): make scraped data ready to push

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const fetch = require("node-fetch");
 // const puppeteer = require("puppeteer");
 
+// #1
+// Web scraping mechanism with puppeteer
+
 // (async () => {
 //   const browser = await puppeteer.launch();
 //   const page = await browser.newPage();
 //   await page.goto("https://www.bbc.com");
 //   // await page.screenshot({ path: "example.png" });
 
-//   // scraping mechanism #1
 //   const newsTitles = await page.evaluate(() =>
 //     // scrapes: title, url, summary, category tag and category link
 //     Array.from(document.querySelectorAll("div.media__content")).map((i) => {
@@ -42,11 +44,21 @@ const rssFeeds = [
   "https://moxie.foxnews.com/feedburner/latest.xml", // fox
 ];
 
-for (let i = 0; i < rssFeeds?.length; i++) {
-  let url = baseURL + rssFeeds[i];
-  console.log("\n", "i=", i, "\n", "url ->", url);
+const fetchData = async () => {
+  let scrapedDataToStoreInFirebase = [];
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((response) => console.log(response));
-}
+  // fetch data from each rssFeed source array using loop
+  for (let i = 0; i < rssFeeds?.length; i++) {
+    let url = baseURL + rssFeeds[i];
+    const response = await fetch(url);
+    const data = await response.json();
+    scrapedDataToStoreInFirebase.push(data);
+  }
+
+  console.log(scrapedDataToStoreInFirebase);
+
+  // TODO: @Poojan setup firebase and try to push this array -> scrapedDataToStoreInFirebase to Firebase
+  // make a function call here to trigger firebase push
+};
+
+fetchData();
